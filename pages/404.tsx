5496c3ae4ff7f17f0f2c3ea4268d10ed9118bd1c
xkvbikx <xkvbikx@gmail.com>
2022-10-30 15:57:59 +0100

    improve style

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,35 +1,41 @@
 import { Home } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
+import { Container } from "@mui/system";
 import { NextPage } from "next";
 import Head from "next/head";
 
 import MyLinkButton from "../src/components/my/MyLinkButton";
 
 const NotFound: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Brak strony</title>
-        <meta
-          name="description"
-          content="Strona o podanym adresie nie istnieje"
-        />
-      </Head>
-      <Box mt={15} textAlign="center">
-        <Typography variant="h4">Ta strona jest niedostępna</Typography>
-        <Typography mt={10} mb={5} variant="h5">
-          Link może być uszkodzony lub strona mogła zostać usunięta.
-          <br /> Sprawdź, czy link, który próbujesz otworzyć, jest poprawny.
-        </Typography>
+    return (
+        <>
+            <Head>
+                <title>Strona nie znaleziona</title>
+                <meta name="description" content="Strona nie znaleziona." />
+            </Head>
+            <Container
+                component="main"
+                sx={{
+                    px: { xs: 1, sm: 10, md: 5, lg: 15, xl: 20 },
+                }}
+            >
+                <Box textAlign="center" mt={5}>
+                    <Typography variant="h3">Ta strona jest niedostępna</Typography>
+                    <Typography mt={"10%"} mb={5} variant="h5">
+                        Link może być uszkodzony lub strona mogła zostać usunięta.
+                        <br /> Sprawdź, czy link, który próbujesz otworzyć, jest poprawny.
+                    </Typography>
 
-        <MyLinkButton
-          isActive={false}
-          href="/"
-          text="Powrót do strony głównej"
-          Icon={Home}
-        />
-      </Box>
-    </>
-  );
+                    <MyLinkButton
+                        text="Powrót do strony głównej"
+                        href="/"
+                        isActive={false}
+                        size="large"
+                        Icon={Home}
+                    />
+                </Box>
+            </Container>
+        </>
+    );
 };
 export default NotFound;
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,37 +1,44 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Head from "next/head";
 
 function Contact() {
-  return (
-    <>
-      <Head>
-        <title>Kontakt</title>
-        <meta
-          name="description"
-          content="Strona kontaktowa. Tutaj znajdziesz dane kontaktowe"
-        />
-        <link rel="canonical" href="/contact" />
-      </Head>
-      <Box mt={15} textAlign="center">
-        <Typography variant="h1">Kontakt</Typography>
-        <Typography mt={10} mb={5} variant="h5">
-          Znalazłeś błąd lub masz pytanie.
-          <br /> Napisz mail.
-          <br /> Staram sie odpisywać jak najszybciej
-          <br />
-          <br />
-          <br /> Chełmscy emeryci
-          <br /> 20-100 Chełm, Polska
-          <br />
-          <a href="mailto:name@email.com">???@gmail.com</a>
-          <br />
-          <br />
-          <br /> Autorem strony jest Jakub Marchwiany
-          <br />
-          <a href="mailto:name@email.com">jacobmarchwiany@gmail.com</a>
-        </Typography>
-      </Box>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Kontakt</title>
+                <meta
+                    name="description"
+                    content="Strona kontaktowa. Tutaj znajdziesz dane kontaktowe"
+                />
+                <link rel="canonical" href="/contact" />
+            </Head>
+            <Container
+                component="main"
+                sx={{
+                    px: { xs: 5, sm: 30, md: 15, lg: 30, xl: 40 },
+                }}
+            >
+                <Box textAlign="center">
+                    <Typography variant="h1">Kontakt</Typography>
+                    <Typography mt={10} mb={5} variant="h5">
+                        Znalazłeś błąd lub masz pytanie.
+                        <br /> Napisz mail.
+                        <br /> Staram sie odpisywać jak najszybciej
+                        <br />
+                        <br />
+                        <br /> Chełmscy emeryci
+                        <br /> 20-100 Chełm, Polska
+                        <br />
+                        <a href="mailto:name@email.com">???@gmail.com</a>
+                        <br />
+                        <br />
+                        <br /> Autorem strony jest Jakub Marchwiany
+                        <br />
+                        <a href="mailto:name@email.com">jacobmarchwiany@gmail.com</a>
+                    </Typography>
+                </Box>
+            </Container>
+        </>
+    );
 }
 export default Contact;
