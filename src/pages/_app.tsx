1d5f333ab9afa81b6d50b2dabb3822c809e1f0dc
xkvbikx <jakubmarchwiany@icloud.com>
2023-06-02 19:17:25 +0200

    come back to project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,63 +1,57 @@
-import { CacheProvider, EmotionCache } from "@emotion/react";
-import { Box, Stack } from "@mui/material";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import { AppProps } from "next/app";
-import Head from "next/head";
-import { Toaster } from "react-hot-toast";
-import "../assets/styles.css";
-import { theme } from "../assets/theme";
-import Footer from "../layout/Footer";
-import Navbar from "../layout/Navbar";
-import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { Box, Stack } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Toaster } from 'react-hot-toast';
+import '../assets/styles.css';
+import { theme } from '../assets/theme';
+import Footer from '../layout/Footer';
+import Navbar from '../layout/Navbar';
+import createEmotionCache from '../utils/createEmotionCache';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-interface MyAppProps extends AppProps {
-    emotionCache?: EmotionCache;
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
 }
 
 export default function MyApp(props: MyAppProps) {
-    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
 
-    return (
-        <CacheProvider value={emotionCache}>
-            <Head>
-                <meta name="viewport" content="initial-scale=1, width=device-width" />
-            </Head>
-            <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                <CssBaseline />
-                <Stack
-                    minHeight={"100vh"}
-                    display="flex"
-                    flexDirection="column"
-                    className="background"
-                >
-                    <Navbar />
-                    <Box
-                        component={"main"}
-                        flex={1}
-                        overflow="auto"
-                        color="primary.contrastText"
-                        py={{ xs: 1, sm: 2, lg: 3 }}
-                    >
-                        <Component {...pageProps} />
-                    </Box>
-                    <Footer />
-                </Stack>
-                <Toaster
-                    position="bottom-center"
-                    gutter={10}
-                    containerStyle={{ marginBottom: "40px" }}
-                    toastOptions={{
-                        style: {
-                            maxWidth: "500px",
-                        },
-                    }}
-                />
-            </ThemeProvider>
-        </CacheProvider>
-    );
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Stack minHeight={'100vh'} display="flex" flexDirection="column" className="background">
+          <Navbar />
+          <Box
+            component={'main'}
+            flex={1}
+            overflow="auto"
+            color="primary.contrastText"
+            py={{ xs: 1, sm: 2, lg: 3 }}
+          >
+            <Component {...pageProps} />
+          </Box>
+          <Footer />
+        </Stack>
+        <Toaster
+          position="bottom-center"
+          gutter={10}
+          containerStyle={{ marginBottom: '40px' }}
+          toastOptions={{
+            style: {
+              maxWidth: '500px',
+            },
+          }}
+        />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
