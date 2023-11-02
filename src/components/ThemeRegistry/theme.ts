187f4d8a4e18e49dd3f367c841afcac79f6657a2
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-02 12:17:01 +0100

    feat: set default textfield color to white

--- src/components/ThemeRegistry/theme.ts
+++ src/components/ThemeRegistry/theme.ts
@@ -28,8 +28,35 @@ const theme = responsiveFontSizes(
 				lg: 1216,
 				xl: 1408
 			}
+		},
+		components: {
+			MuiInputBase: {
+				styleOverrides: {
+					root: {
+						color: "#fff"
+					}
+				}
+			},
+			MuiCssBaseline: {
+				styleOverrides: {
+					body: {
+						backgroundColor: "#133478"
+					}
+				}
+			},
+			MuiTooltip: {
+				styleOverrides: {
+					tooltip: {
+						fontSize: "1rem",
+						maxWidth: "50vw"
+					}
+				}
+			}
 		}
 	})
 );
+
+export const standardSize = { xs: "95%", sm: "50%", md: "40%", lg: "35%", xl: "25%" };
+export const panelStandardSize = { xs: "95%", sm: "80%", md: "70%", lg: "60%", xl: "50%" };
 
 export default theme;
