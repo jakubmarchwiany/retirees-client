0824492d05fced73b08ab3e9c217eef9a5c3b91e
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-03 20:35:02 +0100

    feat: add function to send post

--- src/app/admin/post/create/page.tsx
+++ src/app/admin/post/create/page.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client";
 
 import DateForm from "@/components/create_post/DateForm";
 import { ImageSelector } from "@/components/create_post/ImageSelector";
 import TextEditor from "@/components/create_post/TextEditor";
 import Post from "@/components/post/Post";
-import { Stack, TextField, Typography } from "@mui/material";
+import { postValidation } from "@/components/post/post.validation";
+import { formDataFetch } from "@/utils/fetches";
+import { Button, Stack, TextField, Typography } from "@mui/material";
 import { Dayjs } from "dayjs";
 import { useState } from "react";
 
@@ -15,6 +19,27 @@ export default function NewPostPage(): JSX.Element {
 	const [endDate, setEndDate] = useState<Dayjs | undefined>(undefined);
 	const [cropImage, setCropImage] = useState<string | undefined>(undefined);
 	const [content, setContent] = useState("");
+
+	const addPost = (): void => {
+		if (postValidation(title, startDate, content)) {
+			const body = new FormData() as FormData;
+
+			body.set("title", title);
+
+			body.set("startDate", startDate!.toString());
+
+			if (endDate !== undefined) {
+				body.set("endDate", endDate.toString());
+			}
+			if (cropImage !== undefined) {
+				body.set("image", cropImage);
+			}
+
+			body.set("content", content);
+
+			formDataFetch(body, "/admin/posts/-/create").then(() => {});
+		}
+	};
 
 	return (
 		<Stack alignItems="center">
@@ -59,6 +84,15 @@ export default function NewPostPage(): JSX.Element {
 				startDate={startDate !== undefined ? startDate.toString() : "Brak daty"}
 				title={title}
 			/>
+
+			<Button
+				color="success"
+				onClick={addPost}
+				sx={{ mt: 2, width: { xs: "95%", sm: "80%", md: "70%", lg: "60%", xl: "50%" } }}
+				variant="contained"
+			>
+				Dodaj Post
+			</Button>
 		</Stack>
 	);
 }
