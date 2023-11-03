346f622d5f8c0ad928a151304bc0e82e96eed7f3
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-03 20:36:25 +0100

    feat: add google bucket handler

--- src/environment.d.ts
+++ src/environment.d.ts
@@ -3,7 +3,10 @@ namespace NodeJS {
 	interface ProcessEnv {
 		ADMIN_PASSWORD: string;
 		ADMIN_USERNAME: string;
+		GOOGLE_BUCKET_NAME: string;
 		JWT_SECRET: string;
+		KEY_FILE_NAME: string;
 		NEXT_PUBLIC_API_ENDPOINT: string;
+		PROJECT_ID: string;
 	}
 }
