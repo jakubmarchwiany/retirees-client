881b9c247f9769e67f45b40cf73809ad43005371
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-04 11:43:06 +0100

    feat: add create api endpoint

--- src/app/api/admin/posts/-/create/route.ts
+++ src/app/api/admin/posts/-/create/route.ts
@@ -1,44 +1,55 @@
 import { validateObject } from "@/middlewares/validate_object";
+import { Post } from "@/types/post.type";
 import { createResponse } from "@/utils/create_response";
-import { getErrorMessage } from "@/utils/get_error_message";
+import {
+	downloadPostsFromBucket,
+	savePostsToBucket,
+	uploadPostImageToBucket
+} from "@/utils/google_bucket.api";
 import { NextResponse } from "next/server";
 
 import { CreatePostData, createPostDataSchema } from "./create_post.schema";
-
-export const config = {
-	api: {
-		bodyParser: false
-	}
-};
 
 export async function POST(req: Request): Promise<NextResponse> {
 	try {
 		const data = await req.formData();
 
-		const image = data.get("image") as File | null;
+		const file = data.get("file") as File | null;
 
-		const post = {
+		const dataToCheck = {
 			title: data.get("title"),
 			startDate: data.get("startDate"),
-			endDate: data.get("endDate"),
+			endDate: data.get("endDate") === null ? undefined : data.get("endDate"),
 			content: data.get("content")
 		};
 
 		const { title, startDate, endDate, content } = validateObject<CreatePostData>(
-			post,
+			dataToCheck,
 			createPostDataSchema
 		);
 
-		if (image !== null) {
-			const bytes = await image.arrayBuffer();
+		const postsData = await downloadPostsFromBucket();
+
+		let newPost: Post;
+
+		if (file !== null) {
+			const bytes = await file.arrayBuffer();
 
 			const buffer = Buffer.from(bytes);
 
-			console.log(buffer);
+			const imageName = await uploadPostImageToBucket(buffer);
+
+			newPost = { title, startDate, endDate, imageName, content };
+		} else {
+			newPost = { title, startDate, endDate, content };
 		}
+
+		postsData.unshift(newPost);
+
+		await savePostsToBucket(postsData);
 
 		return createResponse(200, "Udało się dodać post");
 	} catch (error) {
-		return createResponse(400, getErrorMessage(error));
+		return createResponse(400, "Nie udało się dodać posta");
 	}
 }
