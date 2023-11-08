8c7d485a528bfc901f38e9cf17a768bb0bee3636
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-08 15:50:18 +0100

    feat(contact page): improve style

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,45 +1,38 @@
 import { Box, Container, Typography } from "@mui/material";
-import Head from "next/head";
 
 export default function ContactPage(): JSX.Element {
 	return (
-		<>
-			<Head>
-				<title>Kontakt</title>
-				<meta
-					content="Strona kontaktowa. Tutaj znajdziesz dane kontaktowe"
-					name="description"
-				/>
-				<link href="/contact" rel="canonical" />
-			</Head>
-			<Container
-				component="main"
-				sx={{
-					px: { xs: 5, sm: 30, md: 15, lg: 30, xl: 40 }
-				}}
-			>
-				<Box textAlign="center">
-					<Typography variant="h1">Kontakt</Typography>
-					<Typography mt="5%" variant="h5">
-						Masz pytanie napisz maila na adres:
-					</Typography>
-					<Typography mt="2.5%" variant="h5">
-						<a href="mailto:name@email.com">chelmscyemerycisw@gmail.com</a>
-					</Typography>
-					<Typography mt="5%" variant="h5">
-						Adres: <br />
-						Zarząd koła
-						<br /> 20-100 Chełm,
-						<br /> Polska ul. Kolejowa 112
-					</Typography>
-					<Typography mt="15%" variant="h5">
-						Autorem strony jest Jakub Marchwiany
-					</Typography>
-					<Typography mt="2.5%" variant="h5">
-						<a href="mailto:name@email.com">jakubmarchwiany@icloud.com</a>
-					</Typography>
-				</Box>
-			</Container>
-		</>
+		<Container
+			component="main"
+			sx={{
+				px: { xs: 5, sm: 30, md: 15, lg: 30, xl: 40 }
+			}}
+		>
+			<Box textAlign="center">
+				<Typography variant="h1">Kontakt</Typography>
+				<Typography mt="10%" variant="h5">
+					<b>Masz pytanie napisz</b>
+				</Typography>
+
+				<Typography mt="1%" variant="h5">
+					<a href="mailto:name@email.com" style={{ color: "lightgray" }}>
+						chelmscyemerycisw@gmail.com
+					</a>
+				</Typography>
+				<Typography mt="7.5%" variant="h5">
+					<b>Adres</b> <br />
+					Zarząd koła
+					<br /> 20-100 Chełm, ul. Kolejowa 112
+				</Typography>
+				<Typography mt="7.5%" variant="h5">
+					<b>Autor strony</b>
+				</Typography>
+				<Typography mt="1%" variant="h5">
+					<a href="mailto:name@email.com" style={{ color: "lightgray" }}>
+						jakubmarchwiany@icloud.com
+					</a>
+				</Typography>
+			</Box>
+		</Container>
 	);
 }
