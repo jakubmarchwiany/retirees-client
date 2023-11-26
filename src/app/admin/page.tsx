7b806545903920fb7f66c8486a05cd4063792654
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-26 11:02:10 +0100

    feat: add revalidation to admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,46 +2,38 @@ import Post from "@/app/components/post/Post";
 import PostsPagination from "@/app/components/post/PostsPagination";
 import { PostType } from "@/types/post.type";
 import { Box, Stack } from "@mui/material";
+import { unstable_cache } from "next/cache";
 
+import { getPosts } from "../api/db/queries/get_posts";
 import DeleteButton from "../components/post/DeleteButton";
 import EditButton from "../components/post/EditButton";
 
 const { GOOGLE_BUCKET_IMAGES_URL } = process.env;
 
-async function getData(currentPage: number): Promise<{ posts: PostType[]; numberOfPages: number }> {
-	const dataFetch = await fetch(
-		"https://storage.googleapis.com/retirees-chelm.appspot.com/development/posts_data.json",
-		{
-			next: { tags: ["posts_update"] }
+const getCachedPosts = unstable_cache(
+	async (id) => {
+		try {
+			const posts = await getPosts();
+
+			console.log(new Date());
+
+			return { posts };
+		} catch (error) {
+			return { posts: [] };
 		}
-	);
+	},
+	["get-posts-id"],
+	{ tags: ["posts_update"] }
+);
 
-	const posts = (await dataFetch.json()) as PostType[];
-
-	const startIndex = currentPage === 1 ? 0 : (currentPage - 1) * 5;
-
-	let trimPosts = posts.slice(startIndex, startIndex + 5);
-	const numberOfPages = Math.ceil(posts.length / 5);
-
-	trimPosts = trimPosts.map((p) => {
-		if (p.image !== undefined) {
-			return { ...p, image: GOOGLE_BUCKET_IMAGES_URL + p.image };
-		} else {
-			return p;
-		}
-	});
-
-	return { posts: trimPosts, numberOfPages };
-}
-
-export default async function HomePage({
+export default async function AdminPage({
 	searchParams
 }: {
 	searchParams: { [key: string]: string | string[] | undefined };
 }): Promise<JSX.Element> {
 	const currentPage = searchParams["currentPage"] ?? "1";
 
-	const { posts, numberOfPages } = await getData(Number(currentPage));
+	const { posts } = await getCachedPosts(Number(currentPage));
 
 	const generatePosts = (): JSX.Element[] => {
 		return posts.map((item) => {
@@ -66,7 +58,7 @@ export default async function HomePage({
 		<Stack alignItems="center" justifyContent="center" spacing={{ xs: 1, sm: 1.5, lg: 2 }}>
 			{generatePosts()}
 
-			<PostsPagination currentPage={Number(currentPage)} numberOfPages={numberOfPages} />
+			<PostsPagination currentPage={Number(currentPage)} numberOfPages={0} />
 		</Stack>
 	);
 }
