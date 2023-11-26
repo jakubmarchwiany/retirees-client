75a5551e1364c490b0d3d17008e7c259eef208a3
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-26 12:59:30 +0100

    fix: no check authorization for admin queries
    - change matcher to include api/admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,5 +19,5 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 }
 
 export const config = {
-	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+	matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"]
 };
