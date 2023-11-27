76425d548c121f96f6b6d1d814f8d9f680b83000
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-11-27 18:20:57 +0100

    fix: wrong address and improve style (margins)

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,38 +1,31 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 
 export default function ContactPage(): JSX.Element {
 	return (
-		<Container
-			component="main"
-			sx={{
-				px: { xs: 5, sm: 30, md: 15, lg: 30, xl: 40 }
-			}}
-		>
-			<Box textAlign="center">
-				<Typography variant="h1">Kontakt</Typography>
-				<Typography mt="10%" variant="h5">
-					<b>Masz pytanie napisz</b>
-				</Typography>
+		<Stack textAlign="center">
+			<Typography variant="h1">Kontakt</Typography>
+			<Typography mt="2.5vh" variant="h5">
+				<b>Masz pytanie napisz</b>
+			</Typography>
 
-				<Typography mt="1%" variant="h5">
-					<a href="mailto:name@email.com" style={{ color: "lightgray" }}>
-						chelmscyemerycisw@gmail.com
-					</a>
-				</Typography>
-				<Typography mt="7.5%" variant="h5">
-					<b>Adres</b> <br />
-					Zarząd koła
-					<br /> 20-100 Chełm, ul. Kolejowa 112
-				</Typography>
-				<Typography mt="7.5%" variant="h5">
-					<b>Autor strony</b>
-				</Typography>
-				<Typography mt="1%" variant="h5">
-					<a href="mailto:name@email.com" style={{ color: "lightgray" }}>
-						jakubmarchwiany@icloud.com
-					</a>
-				</Typography>
-			</Box>
-		</Container>
+			<Typography mt={1.5} variant="h5">
+				<a href="mailto:chelmscyemerycisw@gmail.com" style={{ color: "white" }}>
+					chelmscyemerycisw@gmail.com
+				</a>
+			</Typography>
+			<Typography mt="5vh" variant="h5">
+				<b>Adres</b> <br />
+				Zarząd koła EiRSW
+				<br /> 22-100 Chełm, ul. Kolejowa 112
+			</Typography>
+			<Typography mt="10vh" variant="h5">
+				<b>Autor strony</b>
+			</Typography>
+			<Typography mt={1.5} variant="h5">
+				<a href="mailto:jakubmarchwiany@icloud.com" style={{ color: "white" }}>
+					jakubmarchwiany@icloud.com
+				</a>
+			</Typography>
+		</Stack>
 	);
 }
